6d2220f3c51ae7ffd91c12ff16932607875fbc9f
remcoykema <71710163+remcoykema@users.noreply.github.com>
2024-11-01 13:32:20 +0100

    Convert v1 to v2 (#3)
    * wip
    
    * Initial v2 connector

--- README.md
+++ README.md
@@ -1,15 +1,107 @@
-# HelloID-Conn-Prov-Target-SSRPM
+# HelloID-Conn-Prov-Target-SSRPM-Onboarding
 
-| :information_source: Information |
-|:---------------------------|
-| This repository contains the connector and configuration code only. The implementer is responsible to acquire the connection details such as username, password, certificate, etc. You might even need to sign a contract or agreement with the supplier before implementing this connector. Please contact the client's application manager to coordinate the connector requirements.       |
-<br />
-<p align="center"> 
-  <img src="https://www.tools4ever.nl/connector-logos/ssrpm-logo.png">
+> [!IMPORTANT]
+> This repository contains the connector and configuration code only. The implementer is responsible to acquire the connection details such as username, password, certificate, etc. You might even need to sign a contract or agreement with the supplier before implementing this connector. Please contact the client's application manager to coordinate the connector requirements.
+
+<p align="center">
+  <img src="">
 </p>
-<br />
 
-HelloID Provisioning Target Connector for SSRPM Onboarding
+## Table of contents
 
-# HelloID Docs
+- [HelloID-Conn-Prov-Target-SSRPM-Onboarding](#helloid-conn-prov-target-connectorname)
+  - [Table of contents](#table-of-contents)
+  - [Introduction](#introduction)
+  - [Getting started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Connection settings](#connection-settings)
+    - [Correlation configuration](#correlation-configuration)
+    - [Available lifecycle actions](#available-lifecycle-actions)
+    - [Field mapping](#field-mapping)
+  - [Remarks](#remarks)
+  - [Development resources](#development-resources)
+    - [API endpoints](#api-endpoints)
+    - [API documentation](#api-documentation)
+  - [Getting help](#getting-help)
+  - [HelloID docs](#helloid-docs)
+
+## Introduction
+
+_HelloID-Conn-Prov-Target-SSRPM-Onboarding_ is a _target_ connector. _SSRPM-Onboarding_ provides a set of REST API's that allow you to programmatically interact with its data.  
+
+It imports "Onboarding" information for the specific user into SSRPM.  This allows a user that wants to enroll in ssrpm, but does not know their AD account information, an alternative method of enrolling in ssrpm
+
+## Getting started
+
+### Prerequisites
+
+An installed and configured ssrpm web site.
+The OnboardingToken must be specified int the Web.config file of the SSSRPM website. This token kan be generated in the SSRPM Management console.
+
+
+### Connection settings
+
+The following settings are required to connect to the API.
+
+| Setting  | Description                        | Mandatory |
+| -------- | ---------------------------------- | --------- | 
+| Token    | The onboarding API token to connect to the API | Yes       |
+| BaseUrl  | The URL to the API                 | Yes       |
+
+### Correlation configuration
+
+The correlation configuration is used to specify which properties will be used to match an existing account within _SSRPM-Onboarding_ to a person in _HelloID_.
+For this connector no correlation is done, because the interface does not support "get" operations, and the correlation settins are ignored. 
+
+| Setting                   | Value                             |
+| ------------------------- | --------------------------------- |
+| Enable correlation        | `false`                           |
+| Person correlation field  | `not applicable` |
+| Account correlation field | `not applicable`                  |
+
+> [!TIP]
+> _For more information on correlation, please refer to our correlation [documentation](https://docs.helloid.com/en/provisioning/target-systems/powershell-v2-target-systems/correlation.html) pages_.
+
+### Available lifecycle actions
+
+The following lifecycle actions are available:
+
+| Action                                  | Description                                     |
+| --------------------------------------- | ----------------------------------------------- |
+| create.ps1                              | Imports SSRPM onboarding information pertaining to an AD user Account. With this information the end user is later able to enroll in SSRPM by means of the onboarding method |
+
+
+### Field mapping
+
+The field mapping can be imported by using the _fieldMapping.json_ file.
+These contains
+
+## Remarks  
+
+This connector does not have a genuine lifecycle, there is only a create script that imports onboarding information into SSRPM.  This connector only performs the "import" part of the onboarding process, which enables the end-user to complete the onboarding process by useing the SSRPM web site.
+
+## Development resources
+
+### API endpoints
+
+The following endpoints are used by the connector:
+
+| Endpoint | Description               |
+| -------- | ------------------------- |
+| /onboarding/import |  |
+
+### API documentation
+
+Detailed information about the complete onboarding process can be found in the chapter about Onboarding in the "/Admin Console/Documentation/Web Interface Guide.pdf" that comes with the SSRPM installation.
+
+## Getting help
+
+> [!TIP]
+> _For more information on how to configure a HelloID PowerShell connector, please refer to our [documentation](https://docs.helloid.com/en/provisioning/target-systems/powershell-v2-target-systems.html) pages_.
+
+> [!TIP]
+>  _If you need help, feel free to ask questions on our [forum](https://forum.helloid.com)_.
+
+## HelloID docs
+
 The official HelloID documentation can be found at: https://docs.helloid.com/
--- create.ps1
+++ create.ps1
@@ -1,89 +1,130 @@
-#region Initialize default properties
-$config = ConvertFrom-Json $configuration
-$p = $person | ConvertFrom-Json
-$pp = $previousPerson | ConvertFrom-Json
-$pd = $personDifferences | ConvertFrom-Json
-$m = $manager | ConvertFrom-Json
+#################################################
+# HelloID-Conn-Prov-Target-SSRPM-Onboarding-Create
+# PowerShell V2
+#################################################
 
-$success = $False;
-$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
-#endregion Initialize default properties
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-#region Change mapping here
-    $onboardDate = Get-Date -Format "yyyy-MM-dd";
-    $domain = "t4edev.local";
-
-    $account = [PSCustomObject]@{
-                        Action = "new"
-                        OnboardingToken = $config.token
-                        users = [System.Collections.Generic.List[PSCustomObject]]::new()
-                    };
-    
-    $user = [PSCustomObject]@{
-        Domain = $domain
-        SAMAccountName = $p.Accounts.ActiveDirectory.SamAccountName
-        OnboardingDate = $onboardDate
-        Attributes = [System.Collections.Generic.List[PSCustomObject]]::new()
-    };
-    
-    #Claim ID
-    [void]$user.Attributes.add([PSCustomObject]@{
-                                    Name = "ID"
-                                    Value = $p.ExternalID
-                                    Options = 1
-                                }
-    );
-    
-    #Birth date
-    [void]$user.Attributes.add([PSCustomObject]@{
-                                    Name = "DOB"
-                                    Value = (Get-Date -Date $p.details.birthdate).ToUniversalTime().toString("dd/MM/yyyy")
-                                    Options = 34
-                                }
+#region functions
+function Resolve-SSRPM-OnboardingError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
     )
-    
-    [void]$account.users.Add($user);
-#endregion Change mapping here
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+        if (-not [string]::IsNullOrEmpty($ErrorObject.ErrorDetails.Message)) {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -ne $ErrorObject.Exception.Response) {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                if (-not [string]::IsNullOrEmpty($streamReaderResponse)) {
+                    $httpErrorObj.ErrorDetails = $streamReaderResponse
+                }
+            }
+        }
+        try {
+            $errorDetailsObject = ($httpErrorObj.ErrorDetails | ConvertFrom-Json)
+            # Make sure to inspect the error result object and add only the error message as a FriendlyMessage.
+            # $httpErrorObj.FriendlyMessage = $errorDetailsObject.message
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails # Temporarily assignment
+        } catch {
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails
+        }
+        Write-Output $httpErrorObj
+    }
+}
+#endregion
 
-#region Execute
 try {
-    if(-Not($dryRun -eq $True)) {
-        $response = (Invoke-WebRequest -Uri "$($config.ssrpmServer)/onboarding/import" -Method POST -ContentType "application/json" -Body ($account | ConvertTo-Json -Depth 10) -UseBasicParsing | ConvertFrom-Json).Success
-    }
-    else
-    {
-        $response = $true;
-    }
-     
-    if($response -eq $true)
-    {
-        $success = $True;
-        $auditLogs.Add([PSCustomObject]@{
-                Action = "CreateAccount"
-                Message = "Enrolled in SSRPM onboarding for person [$($user.SAMAccountName)]";
-                IsError = $false;
-            });
-    }
-     
-}
-catch
-{
-        $auditLogs.Add([PSCustomObject]@{
-                Action = "CreateAccount"
-                Message = "Failed to Enroll SSRPM Onboarding: $($_)"
-                IsError = $True
-            });
-        Write-Error "$($_) : General error"
-}
-#endregion Execute 
- 
-#region build up result
-$result = [PSCustomObject]@{
-    Success= $success
-    AccountReference= $user.SAMAccountName
-    AuditLogs= $auditLogs
-    Account = $account
-};
+    # Initial Assignments
+    $outputContext.AccountReference = 'Currently not available'
 
-Write-Output $result | ConvertTo-Json -Depth 10
-#endregion build up result
+    # Validate correlation configuration
+    # there is no "get" functionality for onboarding, so account correlation cannot be done
+
+    $action = 'CreateAccount'
+
+    # Process
+    switch ($action) {
+        'CreateAccount' {
+
+            $onboardDate = Get-Date -Format "yyyy-MM-dd";
+            $user = [PSCustomObject]@{
+                Domain = $actionContext.Data.Domain
+                SAMAccountName = $actionContext.Data.SamAccountName
+                OnboardingDate = $onboardDate
+                Attributes = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+            };
+            $Attributes = $actionContext.Data.Attributes | convertFrom-Json
+            foreach( $Attribute in $Attributes) {
+                [void]$user.Attributes.Add($Attribute);
+            }
+
+            $account = [PSCustomObject]@{
+                Action = "new"
+                OnboardingToken = $actionContext.Configuration.token
+                users = [System.Collections.Generic.List[PSCustomObject]]::new()
+            };
+            [void]$account.users.Add($user);
+
+            $splatCreateParams = @{
+                Uri    = "$($actionContext.Configuration.BaseUrl)/onboarding/import"
+                Method = 'POST'
+                ContentType = "application/json"
+                UseBasicParsing = $true
+                Body   = $account | ConvertTo-Json -Depth 10
+            }
+            # Make sure to test with special characters and if needed; add utf8 encoding.
+            if (-not($actionContext.DryRun -eq $true)) {
+                Write-Information 'Creating and correlating SSRPM-Onboarding account'
+                # < Write Create logic here >
+
+                $createdAccount = Invoke-RestMethod @splatCreateParams
+                if($createdAccount.Success -eq $false)
+                {
+                    throw "Error onboarding ssrpm user [$($createdAccount.ErrorCode)] [$($createdAccount.Errormessage)]"
+                }
+                $outputContext.AccountReference = $actionContext.Data.SamAccountName
+
+            } else {
+                Write-Information '[DryRun] Create and correlate SSRPM-Onboarding account, will be executed during enforcement'
+            }
+            $auditLogMessage = "Create account was successful. AccountReference is: [$($outputContext.AccountReference)]"
+            break
+        }
+    }
+
+
+    $outputContext.success = $true
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Action  = $action
+            Message = $auditLogMessage
+            IsError = $false
+        })
+} catch {
+    $outputContext.success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-SSRPM-OnboardingError -ErrorObject $ex
+        $auditMessage = "Could not create or correlate SSRPM-Onboarding account. Error: $($errorObj.FriendlyMessage)"
+        Write-Warning "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not create or correlate SSRPM-Onboarding account. Error: $($ex.Exception.Message)"
+        Write-Warning "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
+}
